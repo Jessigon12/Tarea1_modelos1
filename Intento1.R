5082414b221c447e1a2af5b609833840b46b3624
Jessica <jekagonzalez1234@gmail.com>
2025-08-31 22:11:59 -0600

    Varianzas

--- Intento1.R
+++ Intento1.R
@@ -11,6 +11,12 @@ function(matriz, vect_fila, vect_columna){
   EXY<-sum(matriz*(x%o%y))#%o% hace todas las combiancaciones de 2 vectores
   
   #Varianzas
+  EX2<-sum(x^2*px)
+  EY2<-sum(y^2*py)
+  
+  VarX <- EX2 - EX^2
+  VarY <- EY2 - EY^2
+  
 }
 
 
